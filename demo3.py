91183ed3708ef924dce9768f32c15165274dc3b2
Bowen <haicheng93@hotmail.com>
2021-06-26 01:17:55 +0800

    2-2

--- demo3.py
+++ demo3.py
@@ -3,13 +3,12 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 import backtrader as bt
 import datetime
 import pandas as pd
-from backtrader import cerebro
 
 
 class MyStrategy(bt.Strategy):#继承bt.Strategy类
     def __init__(self):
-        self.bt_sma = bt.indicators.MovingAverageSimple(self.data) #赋值给this.bt_sma(变量随便起名字)
-
+        self.bt_sma = bt.indicators.MovingAverageSimple(self.data) #赋值给this.bt_sma(随便起名字),plot()函数会自动标出移动平均线
+        #默认画30days均线
 
     def start(self):
         print("start")
@@ -18,16 +17,7 @@ class MyStrategy(bt.Strategy):#继承bt.Strategy类
         print("prenext")
 
     def next(self):#next的策略核心，如果是分钟线，每过1分钟调用一次，日线则每过一天调用一次，取决于数据间隔
-        #print(self.data.close[0]);
-        #self.data就是被feed的data，.close就是close列，[0]可以防止未来函数，[0]表示当前时间点，-1表示昨天/上一分钟，[1]表示明天
-        ma_value = sum([self.data.close[-cnt] for cnt in range(0, 24)]) / 24 #过去24日均价
-        if self.data.close[0] > ma_value and self.data.close[-1] < ma_value:#今天高于&昨天低于5日均价，则买入
-            print('long', self.data.datetime.date(), self.data.close[0])#获取当日日期用.datetime
-            self.order = self.buy(size = 100)#执行买入，买入10000手
-
-        if self.data.close[0] < ma_value and self.data.close[-1] > ma_value:#今天低于&昨天高于5日均价
-            print('short', self.data.datetime.date(), self.data.close[0])#获取当日日期用.datetime
-            self.order = self.sell(size = 100)
+        pass
 
 
     def stop(self):
