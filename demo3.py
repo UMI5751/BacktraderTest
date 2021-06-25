dd96f1b25e6f104aa9177e5113dd32ea04799e74
Bowen <haicheng93@hotmail.com>
2021-06-26 01:35:44 +0800

    2-2

--- demo3.py
+++ demo3.py
@@ -9,6 +9,7 @@ class MyStrategy(bt.Strategy):#继承bt.Strategy类
     def __init__(self):
         self.bt_sma = bt.indicators.MovingAverageSimple(self.data) #赋值给this.bt_sma(随便起名字),plot()函数会自动标出移动平均线
         #默认画30days均线
+        #在指标还不能计算，即未形成时（30天移动均线的前29天）会调用prenext
 
     def start(self):
         print("start")
@@ -17,7 +18,17 @@ class MyStrategy(bt.Strategy):#继承bt.Strategy类
         print("prenext")
 
     def next(self):#next的策略核心，如果是分钟线，每过1分钟调用一次，日线则每过一天调用一次，取决于数据间隔
-        pass
+        ma_value = self.bt_sma[0];#获取当日移动均线价格
+        ma_value_yesterday = self.bt_sma[-1]#之前的判断代码有问题，修正
+
+        if self.data.close[0] > ma_value and self.data.close[-1] < ma_value_yesterday:#今天高于&昨天低于5日均价，则买入
+            print('long', self.data.datetime.date(), self.data.close[0])#获取当日日期用.datetime
+            self.order = self.buy(size = 100)#执行买入，买入10000手
+
+        if self.data.close[0] < ma_value and self.data.close[-1] > ma_value_yesterday:#今天低于&昨天高于5日均价
+            print('short', self.data.datetime.date(), self.data.close[0])#获取当日日期用.datetime
+            self.order = self.sell(size = 100)
+
 
 
     def stop(self):
